9810d42e94def36560137566e9a94378680e646a
Tim Pender <mrtimpender@gmail.com>
2016-08-11 14:10:01 -0600

    voting tally functions working

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ app.controller('RedditController', function($scope){
   $scope.view.posts = [
     {
       title: 'Christian Bale Ale',
-      author: 'Johnny McSassafras',
+      author: 'Johnny Sassafras',
       body: 'Stumptown celiac sustainable cornhole small batch. Normcore church-key portland franzen kitsch, kinfolk cray PBR&B kombucha poutine. Single-origin coffee church-key pug next level. Chartreuse mixtape cornhole, gluten-free knausgaard asymmetrical migas. Brooklyn gochujang celiac kombucha, sartorial sriracha venmo truffaut meggings godard readymade keffiyeh.',
       votes: 5,
       img: 'https://untappd.akamaized.net/photo/2015_11_01/b4beef29cdc006b3313b9806f95cb5c0_320x320.jpg',
@@ -12,8 +12,8 @@ app.controller('RedditController', function($scope){
       date: new Date()
     },
     {
-      title: 'Doctor Philsner Pilsner',
-      author: 'JRR Tolkien',
+      title: "Doctor Phil's Pils",
+      author: 'Oprah',
       body: 'Affogato try-hard gluten-free, crucifix etsy venmo helvetica selvage street art dreamcatcher fanny pack banjo kale chips. Pork belly tumblr photo booth meggings, jean shorts blog kale chips hella VHS selvage lomo skateboard +1 YOLO street art.',
       votes: -1,
       img: 'http://savourwinnipeg.com/wp-content/uploads/2012/07/Phils-Pils.jpg',
@@ -21,5 +21,10 @@ app.controller('RedditController', function($scope){
       date: new Date()
     }
   ];
-
+  $scope.upVote = function(post){
+    return post.votes++
+  };
+  $scope.downVote = function (post){
+    return post.votes--
+  }
 });
